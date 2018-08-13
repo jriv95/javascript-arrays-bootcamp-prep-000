0481af113268447c23c848471ea85e8f7f11dcf6
Joel Rivera <joel.rivera7995@gmail.com>
2018-08-13 17:35:23 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -34,7 +34,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 }
 
 function removeElementFromBeginningOfArray(array){
-  array.slice(0,1);
+  array.slice(1);
   return array;
 }
 
